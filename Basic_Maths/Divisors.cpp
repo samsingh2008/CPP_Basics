530be65886f94fcef3e860310702ff7de13094fc
Samriddhi Singh <samsingh6002@gmail.com>
2026-08-18 00:10:57 +0530

    Find Divisors - Optimized Approach

--- Basic_Maths/Divisors.cpp
+++ Basic_Maths/Divisors.cpp
@@ -9,11 +9,15 @@
 using namespace std;
 vector<int> findDivisors(int a){
     vector<int> v;
-    for(int i=1;i<=a;i++){
+    for(int i=1;i<=sqrt(a);i++){
         if(a%i==0){
             v.push_back(i);
+            if(i!=a/i){
+                v.push_back(a/i);
+            }
         }
     }
+    sort(v.begin(),v.end());
     return v;
 }
 int main(){
