33b155fa541956652685165a3159a1127cc93fae
Samriddhi Singh <samsingh6002@gmail.com>
2026-08-21 23:47:06 +0530

    Optimize bubble sort with early stopping

--- SORTING/bubble_sort.cpp
+++ SORTING/bubble_sort.cpp
@@ -13,12 +13,17 @@
 using namespace std;
 void bubble_sort(int arr[], int n){
     for(int i=0;i<n-1;i++){
+        int didswap=0;
         for(int j=0;j<n-i-1;j++){
             if(arr[j]>arr[j+1]){
                 int temp=arr[j];
                 arr[j]=arr[j+1];
                 arr[j+1]=temp;
+                didswap=1;
             }
+        }
+        if(didswap==0){
+            break;
         }
     }
     cout<<endl<<"sorted array is"<<endl;
